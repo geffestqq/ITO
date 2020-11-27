36d67d2d5f100fd1087528e2777b7f3ec8975cb2
2SRTVF <viktor.amekhin@gmail.com>
2020-11-27 16:03:35 +0300

    fix 1.2.3.4

--- NewDiplom/obj/Debug/netcoreapp3.1/NewDiplom.AssemblyInfo.cs
+++ NewDiplom/obj/Debug/netcoreapp3.1/NewDiplom.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewDiplom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.7616.26549")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.7636.23316")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewDiplom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewDiplom")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.7616.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.7636.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
